4f9372de50575e1436d4f7345bb1b02715cfa90b
Bago0 <jgamadzebagrati@gmail.com>
2024-01-21 15:32:18 +0400

    updated save_info

--- api/save_info.js
+++ api/save_info.js
@@ -1,17 +1,30 @@
 // api/save_info.js
+const fs = require('fs');
+
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'POST') {
+  if (req.method === 'GET') {
+    try {
+      // Your server-side logic here for GET requests
+
+      // For demonstration purposes, let's read the content of clicked_days.txt
+      const logData = fs.readFileSync('clicked_days.txt', 'utf-8');
+      res.status(200).send(logData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  } else if (req.method === 'POST') {
     try {
       const { dateinfo } = req.body;
 
-      // Your server-side logic here
+      // Your server-side logic here for POST requests
 
       const logData = `Selected Day: ${dateinfo}\n`;
-      console.log(logData);
+      fs.appendFileSync('clicked_days.txt', logData);
 
       res.status(200).send('Information saved successfully.');
     } catch (error) {
