a643d88c5fbc41462ed826640852a03f936a9f6f
Bago0 <jgamadzebagrati@gmail.com>
2024-01-21 15:05:32 +0400

    added headers in save_info

--- save_info.php
+++ save_info.php
@@ -1,5 +1,8 @@
 <?php
-// save_info.php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 
 if (isset($_POST['addDate']) && $_POST['addDate'] === 'addDate') {
     $selectedDay = $_POST["selected_day"];
